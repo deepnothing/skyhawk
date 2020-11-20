19803a16ce21821a6691b6716fc4d60fa444e930
deepnothing <65370281+deepnothing@users.noreply.github.com>
2020-11-20 12:38:26 -0500

    Updates

---
+++ precache-manifest.eff094ca72b2a95b9dff4669421ce78c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "079e4cf0f1e923545252acf3a7264b5c",
+    "url": "/skyhawk/index.html"
+  },
+  {
+    "revision": "7ce5fe5b7b333ae29857",
+    "url": "/skyhawk/static/css/2.b145d9db.chunk.css"
+  },
+  {
+    "revision": "7ce5fe5b7b333ae29857",
+    "url": "/skyhawk/static/js/2.38d438e0.chunk.js"
+  },
+  {
+    "revision": "d39c733111772c3a12f015158abbf2aa",
+    "url": "/skyhawk/static/js/2.38d438e0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a46e3ef3ba62d774e0a8",
+    "url": "/skyhawk/static/js/main.f6338a90.chunk.js"
+  },
+  {
+    "revision": "ce6a5f72ef64e9bfcbee",
+    "url": "/skyhawk/static/js/runtime-main.ef012e22.js"
+  }
+]);
